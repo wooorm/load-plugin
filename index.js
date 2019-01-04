3d2132bdd7f16a137f71a9b70ea5e39656b26aa2
Titus Wormer <tituswormer@gmail.com>
2019-01-04 11:44:19 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -16,34 +16,33 @@ var windows = process.platform === 'win32'
 var prefix = windows ? /* istanbul ignore next */ '' : 'lib'
 var globals = path.resolve(npmPrefix, prefix, 'node_modules')
 
-/* istanbul ignore next - If we’re in Electron, we’re running in a modified
- * Node that cannot really install global node modules.  To find the actual
- * modules, the user has to either set `prefix` in their `.npmrc` (which is
- * picked up by `npm-prefix`).  Most people don’t do that, and some use NVM
- * instead to manage different versions of Node.  Luckily NVM leaks some
- * environment variables that we can pick up on to try and detect the actual
- * modules. */
+// If we’re in Electron, we’re running in a modified Node that cannot really
+// install global node modules.  To find the actual modules, the user has to
+// either set `prefix` in their `.npmrc` (which is picked up by `npm-prefix`).
+// Most people don’t do that, and some use NVM instead to manage different
+// versions of Node.  Luckily NVM leaks some environment variables that we can
+// pick up on to try and detect the actual modules.
+/* istanbul ignore next */
 if (electron && nvm && !fs.existsSync(globals)) {
   globals = path.resolve(nvm, '..', prefix, 'node_modules')
 }
 
-/* Load the plug-in found using `resolvePlugin`. */
+// Load the plug-in found using `resolvePlugin`.
 function loadPlugin(name, options) {
   return require(resolvePlugin(name, options) || name)
 }
 
-/* Find a plugin.
- *
- * See also:
- * <https://docs.npmjs.com/files/folders#node-modules>
- * <https://github.com/sindresorhus/resolve-from>
- *
- * Uses the standard node module loading strategy to find $name
- * in each given `cwd` (and optionally the global node_modules
- * directory).
- *
- * If a prefix is given and $name is not a path, `$prefix-$name`
- * is also searched (preferring these over non-prefixed modules). */
+// Find a plugin.
+//
+// See also:
+// <https://docs.npmjs.com/files/folders#node-modules>
+// <https://github.com/sindresorhus/resolve-from>
+//
+// Uses the standard node module loading strategy to find $name in each given
+// `cwd` (and optionally the global `node_modules` directory).
+//
+// If a prefix is given and $name is not a path, `$prefix-$name` is also
+// searched (preferring these over non-prefixed modules).
 function resolvePlugin(name, options) {
   var settings = options || {}
   var prefix = settings.prefix
@@ -60,13 +59,13 @@ function resolvePlugin(name, options) {
     sources = [cwd || process.cwd()]
   }
 
-  /* Non-path. */
+  // Non-path.
   if (name.indexOf(path.sep) === -1 && name.charAt(0) !== '.') {
     if (settings.global == null ? globally : settings.global) {
       sources.push(globals)
     }
 
-    /* Unprefix module. */
+    // Unprefix module.
     if (prefix) {
       prefix = prefix.charAt(prefix.length - 1) === '-' ? prefix : prefix + '-'
 
